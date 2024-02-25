94b82920fab82694c32b526d7e20d2dfefdd6416
sweets256 <liammsarmientoiv@gmail.com>
2024-02-25 23:12:59 +0800

    char creation fix

--- characterCreation.java
+++ characterCreation.java
@@ -6,7 +6,7 @@ public class characterCreation {
 
     private String[] characterClasses = {
         "Vagabond", "Samurai", "Warrior", "Hero", "Astrologer", "Prophet"
-    }
+    };
 
     private String[][] classStats = {
     {"Vagabond", "9", "15", "13", "9", "11", "14", "9"}, 
@@ -18,33 +18,33 @@ public class characterCreation {
     };
 
     public void createCharacter(){
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("========== Character Creation ==========");
+            System.out.println("Enter character name: ");
+            characterName = input.nextLine();
 
-        System.out.println("========== Character Creation ==========");
-        System.out.println("Enter character name: ");
-        String name = input.nextLine();
+            System.out.println("Select a job class: ");
 
-        System.out.println("Select a job class: ");
+            int i;
+            for (i = 0; i < characterClasses.length && i < classStats.length; i++){
+                System.out.println((i + 1) + ". " + characterClasses[i] + " [Level: " + classStats[i][1] 
+                + ", HP: " + classStats[i][2] + ", Dexterity: " + classStats[i][3] + ", Intelligence: " + classStats[i][4] 
+                + ", Endurance: " + classStats[i][5] + ", Strength: " + classStats[i][6] + ", Faith: " + classStats[i][7] + "]");
+            }
 
-        //int i;
-        for (int i = 0; i < characterClasses.length && i < classStats.length; i++){
-            System.out.println(i + 1) + ". " + characterClasses[i] + " [Level: " + classStats[i][1] 
-            + ", HP: " + classStats[i][2] + ", Dexterity: " + classStats[i][3] + ", Intelligence: " + classStats[i][4] 
-            + ", Endurance: " + classStats[i][5] + ", Strength: " + classStats[i][6] + ", Faith: " + classStats[i][7] + "]";
+            int choice = input.nextInt();
+            if (choice > 0 && choice <= characterClasses.length){
+                jobClass = characterClasses[choice - 1];
+            } else {
+                System.out.println("Invalid choice. Please pick from 1 to 6.");
+            }
+
+            System.out.println("Character created successfully!");
+            System.out.println("Character name: " + characterName);
+            System.out.println("Job class " + jobClass);
+
+            input.close();
         }
-
-        int choice = input.nextInt();
-        if (choice > 0 && choice <= characterClasses.length){
-            jobClass = characterClasses[choice - 1];
-        } else {
-            System.out.println("Invalid choice. Please pick from 1 to 6.");
-        }
-
-        System.out.println("Character created successfully!");
-        System.out.println("Character name: " + characterName);
-        System.out.println("Job class " + jobClass);
-
-        input.close();
     }
 
 
