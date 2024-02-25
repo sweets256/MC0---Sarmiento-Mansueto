b97f14f05cfc51abea3c1b2ca01380703a3d687b
sweets256 <liammsarmientoiv@gmail.com>
2024-02-25 21:41:04 +0800

    Added characterCreation method

--- Navigation.java
+++ Navigation.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Navigation {
-    public Navigation(){
+
+    public void titleScreen(){
         Scanner obj = new Scanner(System.in);
         System.out.println("Please choose your option");
         System.out.println("[1] Start");
         System.out.println("[2] Exit");
 
-        int choice = Integer.parseInt(obj.nextLine());
+        int choice = readChoice(obj.nextLine());
 
         switch (choice){
             case 1 -> System.out.println("Welcome to the Game Lobby");
@@ -16,5 +17,18 @@ public class Navigation {
         }
 
         obj.close(); // scanner closer
+    }
+
+    public void characterCreation(){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("[1] Please input your name");
+        System.out.println("[2] Select Job Class");
+        System.out.println("[3] Confirm");
+        System.out.println("[4] Back");
+
+    }
+
+    public int readChoice(String choice){
+        return Integer.parseInt(choice);
     }
 }
