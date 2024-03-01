f80ecba23e45d73c48746c498914d60180dae1df
pichioes <maria_alyssa_mansueto@dlsu.edu.ph>
2024-03-01 22:14:38 +0800

    javaduck

--- AreaInteractionListener.java
+++ AreaInteractionListener.java
@@ -1,4 +1,18 @@
+/**
+ * The AreaInteractionListener interface defines methods for handling area interactions.
+ * Implementing classes can listen for specific events triggered within the game area.
+ */
 public interface AreaInteractionListener {
+
+    /**
+     * Called when the player initiates fast travel within the area.
+     */
     void onFastTravel();
+
+    /**
+     * Called when the player changes to a new area within the game.
+     *
+     * @param newArea the identifier of the new area
+     */
     void onChangeArea(String newArea);
 }
