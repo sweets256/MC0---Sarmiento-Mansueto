6f4f78dac23a81f974f6e33ef9f430d1545d626f
sweets256 <liammsarmientoiv@gmail.com>
2024-03-30 19:25:24 +0800

    dwadwad

--- Area3Grid.java
+++ Area3Grid.java
@@ -176,7 +176,7 @@ public class Area3Grid {
         }
 
         System.out.println("\n========== Player Stats ==========");
-        System.out.println("Health: " + character.getCurrentHealth());
+        System.out.println("Health: " + character.getOriginalHealth());
         System.out.println("Level: " + character.getLevel());
         System.out.println("Runes: " + character.getRunes());
         System.out.println("==================================\n");
